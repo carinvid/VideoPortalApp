d8d47a063fc4bbbad4ef864fbcaf98636276a53b
Carolina Reedy <carinvid@gmail.com>
2022-03-15 14:01:16 -0600

    adding files

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,18 @@
+<template>
+ <SearchInput></SearchInput>
+</template>
+
+<script>
+import SearchInput from "./components/SearchInput.vue"
+
+export default {
+    name: "App",
+    components:{
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
